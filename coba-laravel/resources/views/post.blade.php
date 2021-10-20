4793f5cb4126d344bd51f9d78b273d58ac43aaf1
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2021-10-20 20:31:41 +0700

    Menyelesaikan video 9 tentang Database Seeder

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,10 +3,10 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By: Jagaddhita Arya Koswara in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         {{-- <h5>{{ $post["author"] }}</</h5> --}}
         {!! $post->body !!} 
         {{-- tanda seru diatas untuk membuat paragraf --}}
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -3,14 +3,17 @@
 @section('container')
 
     @foreach($posts as $post)
-        <article class="mb-5">
+        <article class="mb-5 border-bottom pb-4">
             <h2>
-                <a href="/posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">
                      {{ $post->title }}
                 </a>
             </h2>
+            <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
             <!-- <h5>By : {{ $post->author }}</h5> -->
             <p>{{ $post->excerpt }}</p>
+
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more..</a>
         </article>
     @endforeach
 
