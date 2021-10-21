fed052718f9a6293db8bd9d5fd0aaba071d8b00c
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2021-10-21 23:36:04 +0700

    Menyelesaikan video 12 dengan Redesign UI

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        {{-- <h5>{{ $post["author"] }}</</h5> --}}
-        {!! $post->body !!} 
-        {{-- tanda seru diatas untuk membuat paragraf --}}
+<div class="container">
+    <div class="row justify-content-center mb-5">
+        <div class="col-md-8">
+            <h1 class="mb-3">{{ $post->title }}</h1>
+            <p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in 
+            <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+                {{-- <h5>{{ $post["author"] }}</</h5> --}}
 
-    <a href="/blog" class="d-block mt-3">Back to Posts</a>
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+
+                <article class="my-3 fs-5">
+                     {!! $post->body !!} 
+                </article>
+                {{-- tanda seru diatas untuk membuat paragraf --}}
+            <a href="/blog" class="d-block mt-3">Back to Posts</a>
+        </div>
+    </div>
+</div>
+    
+
+    
 @endsection
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -21,13 +21,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" =>'about',
         "name" => "Jagaddhita Arya Koswara",
         "email" => "193040098.jagaddhita@mail.unpas.ac.id",
         "image" => "jagad.jpg"
@@ -43,6 +45,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function() {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -50,6 +53,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
@@ -57,6 +61,7 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'categories',
         'posts' => $author->posts->load('category', 'author')
     ]);
 });
