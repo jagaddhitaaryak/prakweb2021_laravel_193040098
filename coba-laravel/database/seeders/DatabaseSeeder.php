be7aaa3f00ed5774c04581f4f0e55068d63958e4
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2021-11-28 22:02:02 +0700

    Menyelesaikan video 21 (Upload Image) dan 22 (Preview, Update & Delete Image

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -18,11 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         
 
-        // User::create([
-        //     'name' => 'Jagaddhita Arya Koswara',
-        //     'email' => 'jagaddhitaarya@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Jagaddhita Arya Koswara',
+            'username' => 'jagaddhitaaryak',
+            'email' => 'jagaddhitaarya@gmail.com',
+            'password' => bcrypt('password')
+        ]);
 
         // User::create([
         //     'name' => 'Mohamad Fairuz Putra Koswara',
