362ec250efda3a7f61800f6bccb8aacd50cd8398
jagaddhitaaryak <193040098.jagaddhita@mail.unpas.ac.id>
2021-12-02 12:25:57 +0700

    Menyelesaikan video 23 tentang Authorization

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 
 
 /*
@@ -70,3 +71,6 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 ->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('admin');
